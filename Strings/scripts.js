b848af506c8cc992b3106517c1a9901bd0bcd6b9
Milas <milas.denim@proton.me>
2024-11-06 13:18:21 +0100

    Add files via upload

---
+++ Strings/scripts.js
@@ -0,0 +1,23 @@
+
+console.log("Javascript est super");
+console.log("Javascript est facile");
+console.log("C'est Lao Tseu qui a dit:'Lechec est le fondement de la reussite'");
+
+var c = "Afficher un \\ n'est pas evident";
+console.log(c);
+
+console.log("Ceci est la premiere ligne \nCeci est la deuxieme ligne");
+
+console.log(`Un jour le JavaScript n'aura plus de secrets pour moi
+Lire du code ne sera plus une gageure mais une joie
+Mes doigts seront habitués à tous ces symboles particuliers
+Et ma créativité de développeur pourra enfin s'exprimer`);
+
+var prenomClient = "Bob" ;
+
+console.log("Bonjour\t" + prenomClient + `, aujourd'hui nous faisons une promo "la fidélité paye" !\n
+Tom doit écrire une chaîne de caractère qui permet d'écrire cette phrase pour chaque client. Pour
+l'instant il essaye de faire fonctionner son code avec le prénom "Bob".`);
+
+console.log("\tBonjour\t" + prenomClient + `,\n
+    Aujourd'hui nous faisons une promo "la fidelité paye" !`);
